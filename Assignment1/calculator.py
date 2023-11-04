c7d438d86f7bd865e8b32b390464ba8beeb4d7af
Neema Ogao <146372125+NeemaOgao@users.noreply.github.com>
2023-11-04 17:59:46 +0300

    Add files via upload

---
+++ Assignment1/calculator.py
@@ -0,0 +1,14 @@
+#NEEMA OGAO SCT211-0086/2022
+#Ask for the user's name and display it
+username=input("Enter your name: ")
+print("Hello "+username+"!")
+
+#Input two numbers from the user and calculate the sum and the difference
+Number1=input("Enter the first number : ")
+Number2=input("Enter the second number: ")
+Sum=float(Number1)+float(Number2)
+Difference=float(Number1)-float(Number2)
+
+#Display the sum and difference
+print("The sum of "+Number1+" and "+Number2+" is: "+str(Sum))
+print("The difference between "+Number1+" and "+Number2+" is: "+str(Difference))
